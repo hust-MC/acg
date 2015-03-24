9caba71b7e05e5d51e70a2f38ec265933ac9c24c
hust-MC <251887584@qq.com>
2015-03-24 22:36:08 +0800

    2抽象类1接口完全注释

--- src/com/cf/acg/thread/DownloadInterface.java
+++ src/com/cf/acg/thread/DownloadInterface.java
@@ -1,5 +1,13 @@
 package com.cf.acg.thread;
 
+/**
+ * 接口名：DownloadInterface 功能：用于配合httpThread的下载
+ * 用法说明：当主线程调用httpThread.start()启动线程后，
+ * 后回调自己的download方法，完成下载。通常在这里面会调用readContent回调函数用于解析Json数据
+ * 
+ * @author M
+ * 
+ */
 public interface DownloadInterface
 {
 	void download();
