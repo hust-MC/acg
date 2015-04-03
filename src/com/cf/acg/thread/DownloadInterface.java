396d905935121e48998548080f541c49a8ce6ce7
hust-MC <251887584@qq.com>
2015-04-03 17:00:22 +0800

    commit

--- src/com/cf/acg/thread/DownloadInterface.java
+++ src/com/cf/acg/thread/DownloadInterface.java
@@ -5,10 +5,14 @@ package com.cf.acg.thread;
  * 用法说明：当主线程调用httpThread.start()启动线程后，
  * 后回调自己的download方法，完成下载。通常在这里面会调用readContent回调函数用于解析Json数据
  * 
- * @author M
- * 
+ * @author MC
  */
 public interface DownloadInterface
 {
+	/**
+	 * DownloadInterface接口函数，用于回调DownloadInterface接口对象的下载函数，
+	 * 在自身的download函数中完成下载任务
+	 * @author MC
+	 */
 	void download();
 }
